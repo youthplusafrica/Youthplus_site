299478cce20553f1b77e8bac94a0721164243555
Samuel Muli <samuelmuli95@gmail.com>
2025-10-22 15:35:25 +0300

    Replace thumbnails

--- app/content/ventures.ts
+++ app/content/ventures.ts
@@ -130,7 +130,7 @@ export const VENTURES: Venture[] = [
         title: "One on one with Quinta Onditi",
         date: "2025-07-04",
         location: "Youtube",
-        imageSrc: "/images/quinta_intro.png",
+        imageSrc: "/images/quinta_onditi.jpeg",
         description: "Live taping with founders on building in Africa.",
         details: "From data labs to tractors on African farms, our guest has seen it all. In this episode she unpacks the bold career moves that took her from research to venture capital and now, leading partnerships at Hello Tractor.",
         youtubeUrl: "https://youtu.be/F7mF5AswP5c?si=lyy5wOgyHMnggWAD",
@@ -150,7 +150,7 @@ export const VENTURES: Venture[] = [
         title: "One on one with Andrew Muriuki",
         date: "2025-06-30",
         location: "Youtube",
-        imageSrc: "/images/andrew_muriuki.png",
+        imageSrc: "/images/andrew_muriuki.jpeg",
         details: "In this episode of Connect One On One, we sit down with Andrew Muriuki, the visionary CEO of BARBAH Games, to explore the future of E-Sports in Africa and how gaming can be a powerful tool for youth empowerment, education, and achieving the Sustainable Development Goals (SDGs). Whether you're a gamer, educator, policymaker, or entrepreneur, this conversation will open your eyes to the potential of gaming in Africa.",
         youtubeUrl: "https://youtu.be/lUSC5ZEK_KA?si=2qEeSGXw7AjafQQq",
         hasFutureEvents: false
@@ -159,7 +159,7 @@ export const VENTURES: Venture[] = [
         title: "One on one with Stephanie Ng'ang'a",
         date: "2025-07-11",
         location: "Youtube",
-        imageSrc: "/images/stephanie_ng'ang'a.png",
+        imageSrc: "/images/stephanie_ng'ang'a.jpeg",
         details: "In this episode, we sit down with the vibrant and insightful Stephanie Ng’ang’a, a rising voice in Kenya’s digital content scene and the powerhouse behind the SNS Podcast. From building a personal brand in today’s content-saturated world to navigating the highs and lows of podcasting, Stephanie shares her journey, creative process, and the lessons she’s learned along the way. Whether you’re a budding content creator or a podcast enthusiast, this conversation is packed with inspiration, real talk, and plenty of gems.",
         youtubeUrl: "https://youtu.be/AbsBpnmnVp0?si=ifml4Lzzhygril-H",
         hasFutureEvents: false
@@ -168,7 +168,7 @@ export const VENTURES: Venture[] = [
         title: "One on one with Victor Karanja",
         date: "2025-07-18",
         location: "Youtube",
-        imageSrc: "/images/victor_karanja.png",
+        imageSrc: "/images/victor_karanja.jpeg",
         details: "In this episode of Connect One on One, we sit down with Victor Peace, a storyteller, creative force, and digital content creator who's made a name by turning passion into purpose. Victor opens up about how he got started in the world of content creation, the turning point that helped him realise this could be more than just a hobby, and what it really takes to scale your craft into a full-time career. From early challenges to personal breakthroughs, this conversation dives deep into the mindset, work ethic, and vision behind doing what you love and making a living from it.",
         youtubeUrl: "https://youtu.be/xZdGqWc0F-w?si=J3d6k56DiXbXN9qo",
         hasFutureEvents: false
@@ -177,6 +177,7 @@ export const VENTURES: Venture[] = [
         title: "One on one with Mariah Mariah",
         date: "2025-07-25",
         location: "Youtube",
+        imageSrc: "/images/mariah_mariah.jpeg",
         details: "In this episode, we sit down with Mariah, best known from the Mic Cheque Podcast, for an open and insightful conversation about her journey into the media industry. Mariah shares how she got her start, the obstacles she’s faced along the way, and what it truly takes to thrive in the fast-paced world of media. From navigating challenges as a Black woman in a competitive space to finding her voice and building a platform that resonates, Mariah offers a raw and honest look at both the hustle and the heart behind her success. Whether you're an aspiring creative, a fellow podcaster, or simply someone curious about what it takes to make it in media today, this interview is packed with valuable insights and inspiration. Don’t forget to like, comment, and subscribe for more powerful conversations like this.",
         youtubeUrl: "https://youtu.be/0b2EI5t2r2k?si=-E-GRDN5CBZhh4Ac",
         hasFutureEvents: false
@@ -185,7 +186,7 @@ export const VENTURES: Venture[] = [
         title: "One on one with Grace Akoth",
         date: "2025-08-01",
         location: "Youtube",
-        imageSrc: "/images/grace_akoth.png",
+        imageSrc: "/images/grace_akoth.jpeg",
         details: "In this episode of Connect One-on-One, we sit down with career coach Grace Akoth for an honest and insightful conversation about balancing passion with the need to make a living. Grace shares practical advice on how to build a meaningful career without sacrificing financial stability, and breaks down some of the most common myths surrounding job hunting. Whether you're starting out, changing careers, or simply looking for direction, this episode offers valuable guidance on navigating the modern job market and finding fulfillment in your work.",
         youtubeUrl: "https://youtu.be/HkyrxxlPpgE?si=fIyhYhmGatsiU2VD",
         hasFutureEvents: false
@@ -194,7 +195,7 @@ export const VENTURES: Venture[] = [
         title: "One on one with Amos Ngahu",
         date: "2025-08-08",
         location: "Youtube",
-        imageSrc: "/images/amos_ngahu.png",
+        imageSrc: "/images/amos_ngahu.jpeg",
         details: "Finance coach Amos Ngahu joins Ngina Mwaluko on Connect One On One to share his personal journey with money from struggles to smart financial habits. This episode is packed with real-life lessons on discipline, mindset, and building wealth.",
         youtubeUrl: "https://youtu.be/P5pPQEkrEbo?si=FOwhvX07hJb8hlLA",
         hasFutureEvents: false
@@ -203,7 +204,7 @@ export const VENTURES: Venture[] = [
         title: "One on one with Immanuel Momanyi",
         date: "2025-08-22",
         location: "Youtube",
-        imageSrc: "/images/immanuel_momanyi.png",
+        imageSrc: "/images/immanuel_momanyi.jpeg",
         details: "In this episode of Connect One On One, we sit down with the Head of Accelleration at Villgro Africa, Immanuel Momanyi for a raw and honest conversation about what it really takes to build a startup from the ground up. Forget the glossy headlines, Immanuel shares the unfiltered truths behind scaling challenges, team dynamics, and the emotional highs and why entrepreneurs should aim at solving real problems. Whether you're an aspiring founder, investor, or simply curious about the startup world, this episode delivers practical insights and inspiration you won’t want to miss.",
         youtubeUrl: "https://youtu.be/NeVpVokppOo?si=f_HE5Fy4WxvBPmHu",
         hasFutureEvents: false
@@ -212,6 +213,7 @@ export const VENTURES: Venture[] = [
         title: "One on one with Michael Omuga",
         date: "2025-08-29",
         location: "Youtube",
+        imageSrc: "/images/michael_omugah.jpeg",
         details: "Connect 1 On 1 presents an insightful conversation with Michael Omuga, a leading financial expert, where we dive deep into the crucial financial foundations every startup founder needs to understand. In this episode, Michael shares practical advice on how entrepreneurs can accurately value their companies, manage their finances effectively, and position themselves to attract investors. If you're building a startup or planning to raise capital, this episode offers actionable strategies on understanding startup valuation, avoiding common financial mistakes, and what truly matters to investors when deciding where to put their money. Michael breaks down complex financial concepts into simple, relatable terms, making it easier for founders at any stage to grasp and implement. Whether you're just starting out or looking to scale, this episode is packed with insights that will help you make smarter financial decisions and improve your chances of securing investment. It's a must-watch for startup founders, entrepreneurs, and anyone interested in startup funding, business valuation, or financial literacy in the business world.",
         youtubeUrl: "https://youtu.be/j1RAhaOQ0TQ?si=AD6jGguETxhJ2ben",
         hasFutureEvents: false
